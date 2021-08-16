9a411a6c2d76911727833a960e6d482a1954e198
Palpatio <vesta661@gmail.com>
2021-08-16 15:35:30 +0500

    TodoList/test

--- src/state/tasks-reducer.test.tsx
+++ src/state/tasks-reducer.test.tsx
@@ -25,6 +25,9 @@ test('correct task should be deleted from correct array', () => {
             { id: "2", title: "JS", isDone: true },
             { id: "3", title: "React", isDone: false }
         ],
+
+
+
         "todolistId2": [
             { id: "1", title: "bread", isDone: false },
             { id: "3", title: "tea", isDone: false }
